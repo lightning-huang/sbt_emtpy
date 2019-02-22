3c75598a5741cdddb1e3d7551af64cc547afe623
lightning <huang.lightning@gmail.com>
2019-02-22 22:22:03 +0800

    now we can do step1

--- src/main/scala/MLlib/Word2VecCNPlay.scala
+++ src/main/scala/MLlib/Word2VecCNPlay.scala
@@ -10,11 +10,11 @@ import org.apache.spark.sql.types.{ArrayType, StringType, StructField, StructTyp
 object Word2VecCNPlay {
   def segment(sc:SparkContext): Unit = {
     //stop words
-    val stopWordPath = "停用词路径"
+    val stopWordPath = "stopWords"
     val bcStopWords = sc.broadcast(sc.textFile(stopWordPath).collect().toSet)
 
     //content segment
-    val inPath = "训练语料路径"
+    val inPath = "d:\\data\\news_shard.txt"
     val segmentRes = sc.textFile(inPath)
       .map(AsciiUtil.sbc2dbcCase) //全角转半角
       .mapPartitions(it =>{
@@ -32,35 +32,38 @@ object Word2VecCNPlay {
     })
 
     //save segment result
-    segmentRes.saveAsTextFile("分词结果路径")
+    segmentRes.saveAsTextFile("d:\\data\\cut_txt" )
     bcStopWords.unpersist()
   }
 
   def word2VecRun(sc:SparkContext) = {
     val spark = new SQLContext(sc)
-    val input = sc.textFile("分词结果路径").map(line => line.split(" ").toSeq)
-    val inputDF = spark.createDataFrame(input, Seq[String].getClass)
+    import spark.implicits._
+    case class Text(text: Seq[String])
+    val input = spark.read.text("分词结果路径").map(row => row.getString(0).split(" ").toSeq).map(Tuple1.apply).toDF(colNames="text")
+
 
     //model train
     val word2vec = new Word2Vec()
+      .setInputCol("text")
       .setVectorSize(50)
       .setNumPartitions(64)
 
-    val model = word2vec.fit(inputDF)
-    println("model word size: " + model.getVectors.size)
+    val model = word2vec.fit(input)
+    println("model word size: " + model.getVectors.count())
 
     //Save and load model
-    model.save(sc, "word2vec模型路径")
+    model.save("word2vec模型路径")
     val local = model.getVectors.map{
-      case (word, vector) => Seq(word, vector.mkString(" ")).mkString(":")
-    }.toArray
+      row => Seq(row.getString(0), row.getAs[Vector[Double]](1).mkString(" ")).mkString(":")
+    }.collect()
     sc.parallelize(local).saveAsTextFile("word2vec词向量路径")
 
     //predict similar words
     val like = model.findSynonyms("中国", 40)
-    for ((item, cos) <- like) {
-      println(s"$item  $cos")
-    }
+//    for ((item, cos) <- like) {
+//      println(s"$item  $cos")
+//    }
 
     //val sameModel = Word2VecModel.load(sc, "word2vec模型路径")
   }
@@ -69,8 +72,9 @@ object Word2VecCNPlay {
     System.setProperty("hadoop.home.dir","C:\\winutil")
     val conf = new SparkConf().setAppName("Word2VecCN").setMaster("local[4]")
     val sc = new SparkContext(conf)
-    val spark = new SQLContext(sc)
-    import spark.implicits._
+    segment(sc)
+    //val spark = new SQLContext(sc)
+    //import spark.implicits._
 
 
 
