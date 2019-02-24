357ed87e4643acfe41b892f1393c75d5c95439ba
lightning <huang.lightning@gmail.com>
2019-02-24 20:44:38 +0800

    good util

--- src/main/java/basic/OfCourseUtil.java
+++ src/main/java/basic/OfCourseUtil.java
@@ -1,5 +1,7 @@
 package basic;
 
+import java.io.*;
+
 public class OfCourseUtil {
     public static boolean isNullOrEmpty(String s) {
         return s == null || s.length() == 0;
@@ -9,11 +11,11 @@ public class OfCourseUtil {
         return s == null || s.trim().length() == 0;
     }
 
-    public static boolean isEquals(String a, String b) {
-        return isEquals(a, b, false);
+    public static boolean equals(String a, String b) {
+        return equals(a, b, false);
     }
 
-    public static boolean isEquals(String a, String b, Boolean ignoreCase) {
+    public static boolean equals(String a, String b, Boolean ignoreCase) {
         boolean doubleNull = (a == null) && (b == null);
         boolean singleNull = (a == null && b != null) || (a != null && b == null);
 
@@ -27,5 +29,29 @@ public class OfCourseUtil {
         }
 
         return a.equals(b);
+    }
+
+    public static boolean mkDirs(String path) {
+        return new File(path).mkdirs();
+    }
+
+    public static void removeEverything(String path) {
+        File fileOrDirectory = new File(path);
+
+        if (!fileOrDirectory.exists()) {
+            return;
+        }
+
+        if (fileOrDirectory.isDirectory()) {
+            File[] elements = fileOrDirectory.listFiles();
+
+            if (elements != null && elements.length > 0) {
+                for (File fileOrDirectoryItem : elements) {
+                    removeEverything(fileOrDirectoryItem.getAbsolutePath());
+                }
+            }
+        }
+        // now we have a file or an empty folder, let's kill it
+        fileOrDirectory.delete();
     }
 }
--- src/test/scala/Te.sc
+++ src/test/scala/Te.sc
@@ -1,8 +1,13 @@
+import basic.OfCourseUtil
+
 "Hello"+"World"
 val x=Array("abc","dec")
 val y=x(0)
 "Hi I heard about Spark".split(" ")
 "sdfasdf&34$dfdf:4".split("[\\$&]", 2);
 parallex.ParellexIniReader.PrintTest("abddcd$dfasdf:1&sdf:2=3")
+"sub".substring(1,1)
 
 
+
+OfCourseUtil.RemoveEverything("D:\\data\\a")
